14c636824c71a4225498dbc4bbecaa93e2545bb6
kyhakw <kyhakw@hotmail.com>
2018-07-30 11:02:57 -0500

    setup mlab for heroku deploy

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use(routes);
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
 
